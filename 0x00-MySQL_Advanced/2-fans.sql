12b3f860d363ad038fca14c0d9289ea6670ebceb
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-18 22:02:37 +0200

    bands

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,3 @@
 -- ranks country orderd by number of fans
-SELECT origin AS origin, SUM(fans) AS nb_fans FROM metal_bands
+SELECT origin AS origin, fans AS nb_fans FROM metal_bands
 GROUP BY origin
-ORDER BY SUM(fans)
