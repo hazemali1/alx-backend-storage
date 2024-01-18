3224704d9b937d222ae13a59a039c6a1581ddf20
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-19 00:05:57 +0200

    bands

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -4,7 +4,7 @@ CREATE TRIGGER reset_email BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
 	IF OLD.email != NEW.email THEN
-		UPDATE users SET valid_email = 0;
+		UPDATE users SET NEW.valid_email = 0;
 	END IF;
 END $$
 DELIMITER ;
