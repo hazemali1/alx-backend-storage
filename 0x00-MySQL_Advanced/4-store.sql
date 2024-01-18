52afe0de9924c2ce7fdfd47e7fef66026eb9bd20
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-18 23:24:10 +0200

    bands

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,7 @@
 -- trigger quantity
 DELIMITER $$
 CREATE TRIGGER quantity AFTER INSERT ON orders
+FOR EACH ROW
 BEGIN
 	UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
 END $$
