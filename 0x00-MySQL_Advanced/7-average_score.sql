84b086a3d682e83ad71fdc60308f0d8c83e899e4
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-19 01:20:15 +0200

    Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,13 +1,13 @@
 -- procedure AddBonus
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_idd INT)
 BEGIN
 	DECLARE n INT;
 	DECLARE s INT;
 	DECLARE a float;
-	SELECT COUNT(*) INTO n FROM corrections WHERE user_id = user_id;
-	SELECT SUM(score) INTO s FROM corrections WHERE user_id = user_id;
+	SELECT COUNT(*) INTO n FROM corrections WHERE user_id = user_idd;
+	SELECT SUM(score) INTO s FROM corrections WHERE user_id = user_idd;
 	SET a = s / n;
-	UPDATE users SET average_score = a WHERE id = user_id;
+	UPDATE users SET average_score = a WHERE id = user_idd;
 END $$
 DELIMITER ;
