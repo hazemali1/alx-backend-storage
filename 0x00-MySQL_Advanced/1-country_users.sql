b0e5ff5ced2ed90bdb2a119bae999841c9c010dd
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-18 21:48:45 +0200

    unique

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS users(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
-	country ENUM('US', 'CO', 'TN') NOT NULL default 'US',
+	country ENUM('US', 'CO', 'TN') NOT NULL default 'US'
 )
