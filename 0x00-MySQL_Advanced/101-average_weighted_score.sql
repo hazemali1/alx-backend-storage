5bb0f71ba9bbfa95385f2b02706f1bf9ea314cb5
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-19 04:36:21 +0200

    average_weighted_score

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -9,10 +9,13 @@ BEGIN
 	DECLARE mul FLOAT DEFAULT 0;
 	DECLARE res FLOAT DEFAULT 0;
 	DECLARE myid INT;
+	DECLARE counter INT;
 	DECLARE myweight INT;
 	DECLARE done INT DEFAULT 0;
+	DECLARE id_list VARCHAR(255) DEFAULT '';
 
-	DECLARE mycursor CURSOR FOR SELECT id, weight FROM projects;
+
+	
 
 	DECLARE myuser CURSOR FOR SELECT id FROM users;
 	
@@ -28,18 +31,23 @@ BEGIN
             LEAVE my_loop_user;
         END IF;
 
-		SET num = 0;
-        SET sum = 0;
+		SET id_list = CONCAT(id_list, user_idd, ',');
+
+		FETCH NEXT FROM myuser INTO user_idd;
+	END LOOP my_loop_user;
+	CLOSE myuser;
 
 
+	DECLARE mycursor CURSOR FOR SELECT id, weight FROM projects;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
-		OPEN mycursor;
-		FETCH NEXT FROM mycursor INTO myid, myweight;
-
+	SET counter := 1;
+    SET user_idd := TRIM(SUBSTRING_INDEX(id_list, ',', counter));
+    
+    WHILE LENGTH(user_idd) > 0 DO
 		my_loop: LOOP
 			IF done THEN
 				LEAVE my_loop;
-				SET done = 1;
 			END IF;
 
 			SELECT (score * myweight) INTO mul FROM corrections WHERE project_id = myid AND user_id = user_idd;
@@ -48,14 +56,15 @@ BEGIN
 			FETCH NEXT FROM mycursor INTO myid, myweight;
 		END LOOP my_loop;
 
+		SET counter := counter + 1;
+        SET user_idd := TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(id_list, ',', counter), ',', -1));
+    END WHILE;
+
 		CLOSE mycursor;
 
 		SET res = sum / num;
 		UPDATE users SET average_score = res WHERE id = user_idd;
 
-		FETCH NEXT FROM myuser INTO user_idd;
-	END LOOP my_loop_user;
-	CLOSE myuser;
 END $$
 DELIMITER ;
 
