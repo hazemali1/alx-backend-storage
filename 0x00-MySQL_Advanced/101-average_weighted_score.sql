4a8290c8c8b380a3d74bfb4ab3771434450454cc
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-19 04:52:41 +0200

    average_weighted_score

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -51,6 +51,9 @@ BEGIN
 
 		SET res = sum / num;
 		UPDATE users SET average_score = res WHERE id = user_idd;
+		OPEN mycursor;
+        FETCH FIRST FROM mycursor INTO myid, myweight;
+        CLOSE mycursor;
 
 		FETCH NEXT FROM myuser INTO user_idd;
 	END LOOP my_loop_user;
