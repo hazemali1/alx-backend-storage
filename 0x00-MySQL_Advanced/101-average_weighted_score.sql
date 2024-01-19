2f8e852acd17d8b8976cfd26b3aa5e0b1cc08aee
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-19 04:03:43 +0200

    average_weighted_score

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,56 @@
+-- Average weighted score
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+	DECLARE user_idd INT;
+	DECLARE donee INT DEFAULT 0;
+
+	DECLARE myuser CURSOR FOR SELECT id FROM users;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET donee = 1;
+	OPEN myuser;
+	FETCH NEXT FROM myuser INTO user_idd;
+
+	my_loop: LOOP
+        IF donee THEN
+            LEAVE my_loop;
+        END IF;
+
+		DECLARE num FLOAT DEFAULT 0;
+		DECLARE sum FLOAT DEFAULT 0;
+		DECLARE mul FLOAT DEFAULT 0;
+		DECLARE res FLOAT DEFAULT 0;
+		DECLARE myid INT;
+		DECLARE myweight INT;
+		DECLARE done INT DEFAULT 0;
+
+		DECLARE mycursor CURSOR FOR SELECT id, weight FROM projects;
+		DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+		OPEN mycursor;
+		FETCH NEXT FROM mycursor INTO myid, myweight;
+
+		my_loop: LOOP
+			IF done THEN
+				LEAVE my_loop;
+			END IF;
+
+			SELECT (score * myweight) INTO mul FROM corrections WHERE project_id = myid AND user_id = user_idd;
+			SET sum = sum + mul;
+			SET num = num + myweight;
+			FETCH NEXT FROM mycursor INTO myid, myweight;
+		END LOOP my_loop;
+
+		CLOSE mycursor;
+
+		SET res = sum / num;
+		UPDATE users SET average_score = res WHERE id = user_idd;
+
+		FETCH NEXT FROM myuser INTO user_idd;
+	END LOOP my_loop;
+END $$
+DELIMITER ;
+
+
+
+
+
+
