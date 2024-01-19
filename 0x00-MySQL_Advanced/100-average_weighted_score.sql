3a3ccbf840e3a90bd0ff2c40a9d8af734c0d7882
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-19 03:30:24 +0200

    average_weighted_score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -10,15 +10,20 @@ BEGIN
     DECLARE myweight INT;
 
 	DECLARE mycursor CURSOR FOR SELECT id, weight FROM projects;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 	OPEN mycursor;
 	FETCH NEXT FROM mycursor INTO myid, myweight;
 
-	WHILE @@FETCH_STATUS = 0 DO
+	my_loop: LOOP
+        IF done THEN
+            LEAVE my_loop;
+        END IF;
+
 		SELECT (score * @myweight) INTO mul FROM corrections WHERE project_id = myid AND user_id = user_idd;
 		SET sum = sum + mul
 		SET num = num + myweight
 		FETCH NEXT FROM mycursor INTO myid, myweight;
-	END WHILE;
+	END LOOP my_loop;
 
 	CLOSE mycursor;
 	DEALLOCATE mycursor;
