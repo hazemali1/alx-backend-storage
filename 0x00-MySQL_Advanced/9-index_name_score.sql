e019b0c4033b2cb61795cf5384275065ad66498f
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-19 02:06:57 +0200

    index

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
--- index on name and first and score
-CREATE INDEX idx_name_first_score ON names(name(1));
+-- index on name and score
+CREATE INDEX idx_name_first ON names(name(1));
 CREATE INDEX idx_name_first_score ON names(score(1));
