dcc171445348c70ab975c588df93b3f02b3f3f8a
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-20 17:18:55 +0200

    average_weighted_score

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,65 +1,57 @@
 -- Average weighted score
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_idd INT)
 BEGIN
-	DECLARE user_idd INT;
-
 	DECLARE num FLOAT DEFAULT 0;
 	DECLARE sum FLOAT DEFAULT 0;
 	DECLARE mul FLOAT DEFAULT 0;
 	DECLARE res FLOAT DEFAULT 0;
 	DECLARE myid INT;
-	DECLARE myweight INT;
+    DECLARE myweight INT;
 	DECLARE done INT DEFAULT 0;
 
 	DECLARE mycursor CURSOR FOR SELECT id, weight FROM projects;
-
-	DECLARE myuser CURSOR FOR SELECT id FROM users;
-	
-
-	
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+	OPEN mycursor;
+	FETCH NEXT FROM mycursor INTO myid, myweight;
 
-	OPEN myuser;
-	FETCH NEXT FROM myuser INTO user_idd;
-
-	my_loop_user: LOOP
+	my_loop: LOOP
         IF done THEN
-            LEAVE my_loop_user;
+            LEAVE my_loop;
         END IF;
 
-		SET num = 0;
-        SET sum = 0;
-
-
-
-		OPEN mycursor;
+		SELECT (score * myweight) INTO mul FROM corrections WHERE project_id = myid AND user_id = user_idd;
+		SET sum = sum + mul;
+		SET num = num + myweight;
 		FETCH NEXT FROM mycursor INTO myid, myweight;
+	END LOOP my_loop;
 
-		my_loop: LOOP
-			IF done THEN
-				LEAVE my_loop;
-			END IF;
+	CLOSE mycursor;
 
-			SELECT (score * myweight) INTO mul FROM corrections WHERE project_id = myid AND user_id = user_idd;
-			SET sum = sum + mul;
-			SET num = num + myweight;
-			FETCH NEXT FROM mycursor INTO myid, myweight;
-		END LOOP my_loop;
-
-		CLOSE mycursor;
-
-		SET res = sum / num;
-		UPDATE users SET average_score = res WHERE id = user_idd;
-
-		FETCH NEXT FROM myuser INTO user_idd;
-	END LOOP my_loop_user;
-	CLOSE myuser;
+	SET res = sum / num;
+	UPDATE users SET average_score = res WHERE id = user_idd;
 END $$
 DELIMITER ;
 
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+	DECLARE done INT DEFAULT 0;
+	DECLARE myid INT;
 
+	DECLARE mycursor CURSOR FOR SELECT id FROM users;
+	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+	OPEN mycursor;
+	FETCH NEXT FROM mycursor INTO myid;
 
+	my_loop: LOOP
+        IF done THEN
+            LEAVE my_loop;
+        END IF;
+		CALL ComputeAverageWeightedScoreForUser(myid);
+		FETCH NEXT FROM mycursor INTO myid;
+	END LOOP my_loop;
 
-
-
+	CLOSE mycursor;
+END $$
+DELIMITER ;
