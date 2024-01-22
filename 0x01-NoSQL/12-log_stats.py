36bde88218a07594f0ee2b4229ffce632f261d07
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-23 00:20:12 +0200

    logs

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""pymongo"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    print(collection.count())
+
