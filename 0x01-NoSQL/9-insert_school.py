048516c6ab2e48c09e6aa09fe46f2eb12b300260
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-22 23:31:45 +0200

    find pymongo

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""pymongo"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """insert"""
+    obj = {}
+    for key, value in kwargs.items():
+        obj[key] = value
+    mongo_collection.insert_one(obj)
+    return mongo_collection.insert_one(obj).inserted_id
