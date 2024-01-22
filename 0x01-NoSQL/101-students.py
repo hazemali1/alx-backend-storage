1d587918bd5aa179a489bf863f71e9026577b70a
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-23 01:17:47 +0200

    top student

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,4 +4,4 @@
 
 def top_students(mongo_collection):
     """top students"""
-    return list(mongo_collection.aggregate([{"$group": {"_id": "$_id","name": "$name" , "averageScore": {"$avg": "$score"}}}, {"$sort": {"averageScore": -1}}]))
+    return list(mongo_collection.aggregate([{"$group": {"_id": "$_id", "averageScore": {"$avg": "$score"}}}, {"$sort": {"averageScore": -1}}]))
