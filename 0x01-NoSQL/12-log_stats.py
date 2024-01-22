9cdbe544a81e4ebac48c09f079703e9d7860d8b2
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-23 00:31:03 +0200

    logs

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,3 +8,9 @@ if __name__ == "__main__":
     collection = client.logs.nginx
     print("{} logs".format(collection.count_documents({})))
     print("Methods:")
+    print("    method GET: {}".format(collection.count_documents({"method": "GET"})))
+    print("    method POST: {}".format(collection.count_documents({"method": "POST"})))
+    print("    method PUT: {}".format(collection.count_documents({"method": "PUT"})))
+    print("    method PATCH: {}".format(collection.count_documents({"method": "PATCH"})))
+    print("    method DELETE: {}".format(collection.count_documents({"method": "DELETE"})))
+    print("{} status check".format(collection.count_documents({"method": "GET", "path": "/status"})))
