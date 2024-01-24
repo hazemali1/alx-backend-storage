0f883a9223ae1e4e55bb30795460ae8e3029c3b4
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-25 01:29:18 +0200

    get

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,6 +21,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: typing.Union[str, bytes,  int,  float]) -> str:
         uid = str(uuid.uuid4())
         self._redis.set(uid, data)
