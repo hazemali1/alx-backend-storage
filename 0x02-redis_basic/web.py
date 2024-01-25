f4ffa985016e1cde5ebabeafa6b6e6d6bd8ba4dd
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-25 13:43:20 +0200

    advanced

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,15 +2,13 @@
 """redis"""
 import redis
 import requests
-from bs4 import BeautifulSoup
-
 
 
 def count_urls(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    urls = [a.get('href') for a in soup.find_all('a', href=True)]
-    print(urls)
-    return len(urls)
+    # soup = BeautifulSoup(html_content, 'html.parser')
+    # urls = [a.get('href') for a in soup.find_all('a', href=True)]
+    # print(urls)
+    return len(html_content)
 
 
 def get_page(url: str) -> str:
