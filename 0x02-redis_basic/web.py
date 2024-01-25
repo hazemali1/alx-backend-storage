0841b19d0011d10e1b6d33071578ec2e7abd3a4f
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-25 13:34:17 +0200

    advanced

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,12 +9,13 @@ def count_urls(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     anchor_tags = soup.find_all('a')
     num_urls = len(anchor_tags)
+    print(html_content)
     return num_urls
 
 
 def get_page(url: str) -> str:
     """get page"""
-    return requests.get(url).text
+    return count_urls(requests.get(url).text)
 
 
 if __name__ == "__main__":
