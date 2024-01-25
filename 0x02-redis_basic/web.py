d72554c4feacf1a7b1331e63b8c0ff5ec8d904af
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-25 13:51:41 +0200

    advanced

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,14 +2,19 @@
 """redis"""
 import redis
 import requests
+import re
 
 
 def count_urls(html_content):
     # soup = BeautifulSoup(html_content, 'html.parser')
     # urls = [a.get('href') for a in soup.find_all('a', href=True)]
     # print(urls)
+    url_pattern = re.compile(r'href=["\'](https?://\S+?)["\']', re.IGNORECASE)
+    urls = re.findall(url_pattern, html_content)
+    print(urls)
+
     s = 0
-    if 'href="https://www.google.co"' in html_content:
+    if 'href="https://www.google.com"' in html_content:
         s += 1
     print(s)
     print(type(html_content))
