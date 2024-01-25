b0f2e75e0a20416dff50c3a003f04b8bdd665356
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-25 02:50:39 +0200

    replay

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""redis"""
+import redis
+import requests
+
+
+def get_page(url: str) -> str:
+    """get page"""
+    return requests.get(url).text
+
+if __name__ == "__main__":
+    """main"""
+    get_page('http://slowwly.robertomurray.co.uk')
